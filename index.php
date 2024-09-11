9d54b73a8a4fc7eb8b1981686bb926a0678fa205
Michael Ponce <michaelalexisponce15@gmail.com>
2024-09-11 17:14:05 +0800

    :wrench: Initial Layout

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content=""/>
+    <meta name="keywords" content="" />
+    <meta name="author" content="" />
+    <meta name="robots" content="index, nofollow" />
+
+    <title>Minicell Apparel</title>
+    <link rel="icon" href="https://res.cloudinary.com/dzmhkee5i/image/upload/v1726044546/minicell/cvbz1wok7xzzwydkpklj.png" type="image/icon_type" />
+
+    <!-- Stylesheet -->
+    <link rel="stylesheet" href="styles/header_style.css" />
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+</head>
+<body>
+    <header>
+        <img src="https://res.cloudinary.com/dzmhkee5i/image/upload/v1726044546/minicell/cvbz1wok7xzzwydkpklj.png">
+        <nav>
+            <ul>
+                <li>
+                    <i class="fa-solid fa-magnifying-glass"></i>
+                    <p>Search</p>
+                </li>
+                <li><i class="fa-solid fa-user"></i>
+                <li><i class="fa-solid fa-bag-shopping"></i></li>
+            </li>
+
+            </ul>
+        </nav>
+    </header>
+    <main>
+
+    </main>
+</body>
+</html>
