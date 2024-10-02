c3bcc869012417845cb7a728a6fd0efed8c22fad
Michael Ponce <michaelalexisponce15@gmail.com>
2024-10-03 01:09:23 +0800

    :wrench: Initial landing page

--- index.php
+++ index.php
@@ -1,40 +1,68 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content=""/>
-    <meta name="keywords" content="" />
-    <meta name="author" content="" />
-    <meta name="robots" content="index, nofollow" />
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content=""/>
+        <meta name="keywords" content="" />
+        <meta name="author" content="" />
+        <meta name="robots" content="index, nofollow" />
 
-    <title>Minicell Apparel</title>
-    <link rel="icon" href="https://res.cloudinary.com/dzmhkee5i/image/upload/v1726044546/minicell/cvbz1wok7xzzwydkpklj.png" type="image/icon_type" />
+        <title>Minicell Apparel</title>
+        <!-- <link rel="icon" href="https://res.cloudinary.com/dzmhkee5i/image/upload/v1726044546/minicell/cvbz1wok7xzzwydkpklj.png" type="image/icon_type" /> -->
+        <link rel="icon" href="assets/logo.png" type="image/icon_type" />
 
-    <!-- Stylesheet -->
-    <link rel="stylesheet" href="styles/header_style.css" />
+        <!-- Stylesheet -->
+        <link rel="stylesheet" href="styles/style.css" />
+        <link rel="stylesheet" href="styles/header_style.css" />
+        <link rel="stylesheet" href="styles/landing.css" />
 
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <!-- Fonts -->
+        <link href="https://fonts.cdnfonts.com/css/kulim-park" rel="stylesheet">
+        <link href="https://fonts.cdnfonts.com/css/aclonica" rel="stylesheet">
+        <link href="https://fonts.cdnfonts.com/css/spinnaker" rel="stylesheet">
 
-</head>
-<body>
-    <header>
-        <img src="https://res.cloudinary.com/dzmhkee5i/image/upload/v1726044546/minicell/cvbz1wok7xzzwydkpklj.png">
-        <nav>
-            <ul>
-                <li>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    </head>
+    <body>
+        <header>
+            <img src="assets/logo.png">
+            <nav>
+                <div class="search-bar">
                     <i class="fa-solid fa-magnifying-glass"></i>
-                    <p>Search</p>
-                </li>
-                <li><i class="fa-solid fa-user"></i>
-                <li><i class="fa-solid fa-bag-shopping"></i></li>
-            </li>
+                    <p id="search">Search</p>
+                </div>
+                <div class="buttons">
+                    <i class="fa-solid fa-user"></i>
+                    <i class="fa-solid fa-cart-shopping"></i>
+                </div>
+            </nav>
+        </header>
 
-            </ul>
-        </nav>
-    </header>
-    <main>
-
-    </main>
-</body>
+        <main>
+            <section class="landing-page">
+                <div class="welcome-text">
+                    <div class="welcome-header">
+                        <h1># STYLISH</h1>
+                        <h1>WHITE-TEES</h1>
+                    </div>
+                    <div class="description">
+                        <p>Level up your white-tees with us!</p>
+                        <p>Wear your usual with a style!</p>
+                    </div>
+                </div>
+                <div class="explore-buttons">
+                    <button id="add-cart">
+                        <p>Add to my Cart</p>
+                        <i class="fa-solid fa-arrow-right"></i>
+                    </button>
+                    <button id="explore-now">
+                        <p>Explore Now</p>
+                        <i class="fa-solid fa-arrow-right"></i>
+                    </button>
+                </div>
+            </section>
+        </main>
+    </body>
 </html>
